4f86b0cfb3b3b75be1d3b41945b0b4a761f20596
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-04-10 16:26:58 -0500

    added if/else statements

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            int hour = 10;
+            Console.WriteLine("Aaron Test");
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's Morning.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's Afternoon.");
+            }
+            else
+            {
+                Console.WriteLine("It's Evening");
+            }
         }
     }
 }
