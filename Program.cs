b30a413d69b683f06c25393243e503bada604dae
Aaron Singleterry <aaron.singleterry@claritymis.com>
2022-04-10 17:35:19 -0500

    added switch case

--- Program.cs
+++ Program.cs
@@ -6,23 +6,50 @@ using System.Threading.Tasks;
 
 namespace Conditionals
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            int hour = 10;
-            Console.WriteLine("Aaron Test");
-            if (hour > 0 && hour < 12)
+            //int hour = 10;
+            //if (hour > 0 && hour < 12)
+            //{
+            //    Console.WriteLine("It's Morning.");
+            //}
+            //else if (hour >= 12 && hour < 18)
+            //{
+            //    Console.WriteLine("It's Afternoon.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It's Evening");
+            //}
+            //bool isGoldCustomer = true;
+            //float price;
+
+            //if (isGoldCustomer)
+            //{
+            //    price = 19.95f;
+            //}
+            //else
+            //{
+            //    price = 29.95f;
+            //}
+
+            //price = isGoldCustomer ? 19.95f : 29.95f;
+
+            //Console.WriteLine(price);
+            var season = Season.Autumn;
+
+            switch (season)
             {
-                Console.WriteLine("It's Morning.");
-            }
-            else if (hour >= 12 && hour < 18)
-            {
-                Console.WriteLine("It's Afternoon.");
-            }
-            else
-            {
-                Console.WriteLine("It's Evening");
+                case Season.Autumn:
+                case Season.Summer:
+                    Console.WriteLine("We've got promotion");
+                    break;
+
+                default:
+                    Console.WriteLine("I don't understand that season...");
+                    break;
             }
         }
     }
